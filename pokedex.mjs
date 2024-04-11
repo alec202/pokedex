6e874f3aefe22f616c32b18a5998a705f4a4b3a9
Alec Mirambeau <alecmirambeau@gmail.com>
2024-04-11 11:37:18 -0400

    searching for pokemon work

--- pokedex.mjs
+++ pokedex.mjs
@@ -30,20 +30,47 @@ function showMenu(){
     console.log("3 moveName: will search and display info on move typed in")
 }
 
+function prompt(cb){
+    rl.question("What do you want to search? ", (thing) => {
+        cb(thing)
+    })
+}
 
+function searchPoke(pokemon){
+    let apiConnect = fetch('https://pokeapi.co/api/v2/pokemon/' + pokemon)
+        .then(response => response.json())
+        .then(data => printPoke(data))
+        .catch(error => console.error('Error:', error));
+
+
+}
+
+function printPoke(data){
+    console.log()
+    console.log("Pokemon's name: " + data.name)
+    console.log("Pokemon's weight: " + data.weight)
+    console.log("Pokemon's height: " + data.height)
+    console.log("Pokemon's base experience: " + data.base_experience)
+    console.log("The moves this pokemon can do are:")
+    data.moves.forEach( (moveObject) => {
+        console.log("\t" + moveObject.move.name)
+    })
+    console.log()
+    run()
+
+}
 function run(){
     showMenu()
     rl.question("What is your choice? ", (choice) => {
-        console.log("your choice was:" + choice);
-        let parts = choice.split(" "); // Split the string by space
-        let pokemonName = parts.slice(1)
+        console.log("your choice was: " + choice);
         if (choice[0] == 1){
             // console.log("choice: " + choice[0])
             // console.log("pokemon name: " + pokemonName)
+            prompt(searchPoke)
 
         }
     })
-        console.log("end of question")
 }
+
 
 run()
